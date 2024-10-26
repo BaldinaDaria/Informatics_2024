95d0927f1e3109908b998f7d6cc936116ddc7f89
Балдина Дарья <dashabaldina5300@gmail.com>
2024-10-26 22:20:23 +0300

    Baldina_lab4

---
+++ golang/4lab/4lab.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+
+    bA := 2.5
+    xStart := 1.28
+    xEnd := 3.28
+    deltaX := 0.4
+
+    fmt.Println("Результаты задания A:")
+    for x := xStart; x <= xEnd; x += deltaX {
+        y := (1 + math.Pow(math.Sin(math.Pow(bA, 3)+math.Pow(x, 3)), 2)) / math.Cbrt(math.Pow(bA, 3)+math.Pow(x, 3))
+        fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+    }
+
+    bB := 2.5
+    xValues := []float64{1.1, 2.4, 3.6, 1.7, 3.9}
+
+    fmt.Println("\nРезультаты задания B:")
+    for _, x := range xValues {
+        y := (1 + math.Pow(math.Sin(math.Pow(bB, 3)+math.Pow(x, 3)), 2)) / math.Cbrt(math.Pow(bB, 3)+math.Pow(x, 3))
+        fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+    }
+}
