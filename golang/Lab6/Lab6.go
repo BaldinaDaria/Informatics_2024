9f7defcf34e627f6f26cdc8347c9e4eae6c40cd9
Балдина Дарья <dashabaldina5300@gmail.com>
2024-10-29 16:17:02 +0300

    Lab6

---
+++ golang/Lab6/Lab6.go
@@ -0,0 +1,35 @@
+package Lab6
+
+import (
+	"fmt"
+)
+
+type Cat struct {
+	name  string
+	age   int
+	breed string
+}
+
+func (c *Cat) GetAge() int {
+	return c.age
+}
+
+func (c *Cat) SetAge(age int) {
+	c.age = age
+}
+
+func (c *Cat) Info() string {
+	return fmt.Sprintf("Кошка: %s, Возраст: %d, Порода: %s", c.name, c.age, c.breed)
+}
+
+func RunLab6() {
+
+	myCat := Cat{name: "Барсик", age: 7, breed: "Мейн-кун"}
+
+	fmt.Println("Возраст кошки:", myCat.GetAge())
+
+	myCat.SetAge(8)
+	fmt.Println("Новый возраст кошки:", myCat.GetAge())
+
+	fmt.Println(myCat.Info())
+}
