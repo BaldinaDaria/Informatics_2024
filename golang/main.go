fc4970e7a7253589914b78c9abab71549802937e
BaldinaDaria <dashabaldina5300@gmail.com>
2024-11-23 19:01:22 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	4lab"isuct.ru/informatics2022/4lab"
+4lab"isuct.ru/informatics2022/4lab"
 )
 
 func main() {
 	fmt.Println("Балдина Дарья")
 	4lab.RunLab4()
+	
 }
