2db4739a77dafbffd6460d0c1018aa8e806a0696
BaldinaDaria <dashabaldina5300@gmail.com>
2024-11-30 15:51:27 +0300

    Update 4lab.go

--- golang/4lab/4lab.go
+++ golang/4lab/4lab.go
@@ -9,20 +9,22 @@ func calculateY(b float64, x float64) float64 {
 	return (1 + math.Pow(math.Sin(math.Pow(b, 3)+math.Pow(x, 3)), 2)) / math.Cbrt(math.Pow(b, 3)+math.Pow(x, 3))
 }
 
-func taskA(bA float64, xStart float64, xEnd float64, deltaX float64) {
-	fmt.Println("Результаты задания A:")
+func taskA(bA float64, xStart float64, xEnd float64, deltaX float64) []float64 {
+	results := []float64{}
 	for x := xStart; x <= xEnd; x += deltaX {
 		y := calculateY(bA, x)
-		fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+		results = append(results, y)
 	}
+	return results
 }
 
-func taskB(bB float64, xValues []float64) {
-	fmt.Println("\nРезультаты задания B:")
+func taskB(bB float64, xValues []float64) []float64 {
+	results := []float64{}
 	for _, x := range xValues {
 		y := calculateY(bB, x)
-		fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+		results = append(results, y)
 	}
+	return results
 }
 
 func RunLab4() {
@@ -31,10 +33,20 @@ func RunLab4() {
 	xEnd := 3.28
 	deltaX := 0.4
 
-	taskA(bA, xStart, xEnd, deltaX)
+	resultsA := taskA(bA, xStart, xEnd, deltaX)
+	fmt.Println("Результаты задания A:")
+	for i, y := range resultsA {
+		x := xStart + float64(i)*deltaX
+		fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+	}
 
 	bB := 2.5
 	xValues := []float64{1.1, 2.4, 3.6, 1.7, 3.9}
 
-	taskB(bB, xValues)
+	resultsB := taskB(bB, xValues)
+	fmt.Println("\nРезультаты задания B:")
+	for i, y := range resultsB {
+		x := xValues[i]
+		fmt.Printf("x: %.2f, y: %.4f\n", x, y)
+	}
 }
